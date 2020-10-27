7b142d7de92472656b421242f692c74b656012ff
Amador <amadorherediaroldan@gmail.com>
2020-10-27 18:29:07 +0100

    Matrius

---
+++ PHP_Vector.php
@@ -0,0 +1,16 @@
+<?php
+$weekday[]="Monday";
+$weekday[]="Tuesday";
+$weekday[]="Wednesday";
+$weekday[]="Thursday";
+$weekday[]="Friday";
+$weekday[]="Saturday";
+$weekday[]="Sunday";
+
+for($a=0; $a<count($weekday); $a++)
+{
+echo $weekday[$a];
+echo "<br>";
+}
+
+?>
