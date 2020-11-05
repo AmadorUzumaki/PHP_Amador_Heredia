20a5cc633fa6d310815932f641be2a6f5f73fc2f
AmadorUzumaki <amadorherediaroldan@gmail.com>
2020-11-05 20:23:19 +0100

    prova1

---
+++ PHP_Matriu_Proves.php
@@ -0,0 +1,22 @@
+<?php
+$xdays=array(
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+);
+foreach($xdays as $month) {
+  echo $month;
+  echo "<br>";
+}
+{
+}
+?>
