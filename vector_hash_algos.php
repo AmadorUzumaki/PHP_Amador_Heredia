9b4a70978c05f6826198123bf03e6f7372848dd7
AmadorUzumaki <amadorherediaroldan@gmail.com>
2020-12-10 20:32:51 +0100

    Examen Desembre

---
+++ vector_hash_algos.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Donada la funcio de php hash_algos()
+1-Mostra amb un bucle for each tots els seus elements
+2- de manera ordenada alfabeticament
+3- I mostra amb accés directe per posició als elements amb els valors:
+    sha256
+    md5
+mes informació al link:
+https://www.php.net/manual/en/function.hash-algos.php
+......
+*/
+$vector=hash_algos();
+foreach ($vector as $value){
+  echo "$value<br>";
+}
+echo "<br><br><br>";
+$vector2=hash_algos();
+sort($vector2);
+foreach ($vector2 as $value){
+  echo "$value<br>";
+}
+$vector3=hash_algos();
+echo "<br><br><br>";
+echo "$vector3[5]<br>";
+echo $vector3[2];
+ ?>
